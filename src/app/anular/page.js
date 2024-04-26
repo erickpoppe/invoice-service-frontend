025c006b6698fd2edb750d162c58cc4868c0a3cd
Erick Poppe <erickpoppe@gmail.com>
2024-04-26 16:10:33 -0400

    Fix cancel invoice

--- src/app/anular/page.js
+++ src/app/anular/page.js
@@ -31,7 +31,7 @@ export default function ShippingAddressPage() {
                 }
             })
             .catch((error) => {
-                alert(`Error anulando la factura: ${error}`);
+                alert(`Error anulando la factura: ${error.response.data.detail}`);
             });
     };
 
